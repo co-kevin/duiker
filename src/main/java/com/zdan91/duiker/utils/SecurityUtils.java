d216b4ffe29345252cb7e36cac38c2671d6a7b92
hookszhang <hookszhang@gmail.com>
2017-11-27 13:54:07 +0800

    Add file security utils

---
+++ src/main/java/com/zdan91/duiker/utils/SecurityUtils.java
@@ -0,0 +1,24 @@
+package com.zdan91.duiker.utils;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 获得当前登录用户信息的安全工具类
+ * TODO 实现逻辑
+ *
+ * @author hookszhang
+ */
+public class SecurityUtils {
+
+    public static int getCurrentUserId(HttpServletRequest request) {
+        return -1;
+    }
+
+    public static String getCurrentUserPhoneNumber(HttpServletRequest request) {
+        return null;
+    }
+
+    public static String getCurrentUserName(HttpServletRequest request) {
+        return null;
+    }
+}
