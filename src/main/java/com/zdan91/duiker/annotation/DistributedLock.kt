647d3df6eca3a5b4b5c30178710ef19fad316226
hookszhang <hookszhang@gmail.com>
2017-11-29 19:48:41 +0800

    Update DistributedLock.kt

--- src/main/java/com/zdan91/duiker/annotation/DistributedLock.kt
+++ src/main/java/com/zdan91/duiker/annotation/DistributedLock.kt
@@ -1,13 +1,11 @@
 package com.zdan91.duiker.annotation
 
-import java.lang.annotation.Documented
-
 /**
  * 分布式 Redis 锁, 自动在方法执行前上锁，方法执行后解锁
  * TODO 实现锁的逻辑
  *
  * @author hookszhang
  */
-@Documented
+@MustBeDocumented
 @Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
 annotation class DistributedLock(val key: String = "duiker-lock-key")
