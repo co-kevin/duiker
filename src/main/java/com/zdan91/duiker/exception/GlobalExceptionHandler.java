bc0c93d7f823bdf49abcd35e48781171cf8d4ae7
shijikun <shijikun@91zdan.com>
2017-12-20 19:12:25 +0800

    异常处理

--- src/main/java/com/zdan91/duiker/exception/GlobalExceptionHandler.java
+++ src/main/java/com/zdan91/duiker/exception/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.zdan91.duiker.exception;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.AbstractHandlerExceptionResolver;
@@ -11,13 +14,14 @@ import java.io.IOException;
 @Component
 public class GlobalExceptionHandler extends AbstractHandlerExceptionResolver {
 
+    private Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
     @Override
     protected ModelAndView doResolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
         if (ex instanceof CustomException) {
             try {
                 response.sendError(((CustomException) ex).getCode());
             } catch (IOException e) {
-                e.printStackTrace();
+                log.error("写入异常信息时出错 :", e);
             }
         } else {
             return null;
