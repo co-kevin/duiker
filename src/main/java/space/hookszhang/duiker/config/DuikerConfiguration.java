8429bc2dd05ec3edb4acf8e19829c3a60812efbd
hookszhang <hookszhang@gmail.com>
2018-03-27 10:04:45 +0800

    Update build.gradle, remove sleuth utils

--- src/main/java/space/hookszhang/duiker/config/DuikerConfiguration.java
+++ src/main/java/space/hookszhang/duiker/config/DuikerConfiguration.java
@@ -1,7 +1,6 @@
 package space.hookszhang.duiker.config;
 
 import space.hookszhang.duiker.config.swagger.SwaggerConfiguration;
-import space.hookszhang.duiker.utils.LogbackUtils;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -10,7 +9,4 @@ import org.springframework.context.annotation.Import;
 @ComponentScan
 @Import(SwaggerConfiguration.class)
 public class DuikerConfiguration {
-    static {
-        LogbackUtils.prepareTraceIdFilter();
-    }
 }
